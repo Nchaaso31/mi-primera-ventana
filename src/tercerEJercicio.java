fa9664552cbe5e41e9e46200ce9bfe9da2b45997
Nchaaso31 <josea.nchasobitorosa@iesf.onmicrosoft.com>
2022-09-16 11:07:58 +0200

    Tercer Ejercicio

---
+++ src/tercerEJercicio.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class tercerEJercicio {
+    public static void main(String[] args) {
+        JFrame f =new JFrame();
+        f.setBounds(10,10,100,100);
+        f.setVisible(true);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel mainpanel = new JPanel();
+
+        mainpanel.setBackground(Color.blue);
+
+    }
+}
