3cd9b7c6511ec6e6b10349986e63effb020c44cb
Nchaaso31 <josea.nchasobitorosa@iesf.onmicrosoft.com>
2022-09-16 10:53:44 +0200

    Mi primera ventana panelesss

--- src/segundoEJercicio.java
+++ src/segundoEJercicio.java
@@ -1,15 +1,33 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class segundoEJercicio {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
-        frame.setBounds(40,80,200,200);
+        frame.setBounds(10, 10, 2000, 300);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel mainpanel = new JPanel();
-        mainpanel.setBounds(40,80,200,200);
+        mainpanel.setBackground(Color.white);
+        mainpanel.setLayout(null);
 
+        JPanel panel1 = new JPanel();
+        panel1.setBounds(0, 0, 100, 100);
+        panel1.setBackground(Color.black);
+
+
+        JPanel panel2 = new JPanel();
+        panel2.setBounds(200, 0, 100, 100);
+        panel2.setBackground(Color.yellow);
+
+
+        mainpanel.add(panel1);
+        mainpanel.add(panel2);
+
+        frame.setContentPane(mainpanel);
+        frame.revalidate();
+        frame.repaint();
 
     }
 
