d9fb483abfa5c4cd1867637ad5e8c4450b83c986
Nchaaso31 <josea.nchasobitorosa@iesf.onmicrosoft.com>
2022-09-19 10:43:18 +0200

    Corrección

---
+++ src/myRadioButtonWindowsCorregido.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class myRadioButtonWindowsCorregido {
+    public static void main(String[] args) {
+        JFrame j = new JFrame();
+        j.setBounds(10,10,300,400);
+        j.setVisible(true);
+        j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+        JRadioButton f = new JRadioButton("LINUX");
+
+
+
+    }
+}
